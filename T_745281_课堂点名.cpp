bc14027fd0abe124a32cff16cc576915b8badda9
LuYuxuan <ASUS@LAPTOP-T9K3FD3O>
2026-03-27 21:51:44 +0800

    洛谷入门赛E题，这个题还没有懂！！！

---
+++ T_745281_课堂点名.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m, x, y;
+    cin >> n >> m >> x >> y;
+    int a[1005][1005];
+    //输入矩阵
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            cin >> a[i][j];
+
+
+    int early = 0;
+    int late = 0;  
+
+    for (int i = x + 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            if (a[i][j] == 1)
+                early++;
+    early++; 
+
+    for (int i = x; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            if (a[i][j] == 1)
+                late++;
+
+    cout << early << " " << late << endl;
+    return 0;
+}
