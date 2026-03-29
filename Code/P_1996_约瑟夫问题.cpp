bab929ad9b1b761f961504ed7e519bb9cb1124c0
LuYuxuan <ASUS@LAPTOP-T9K3FD3O>
2026-03-29 21:09:21 +0800

    P1996

---
+++ Code/P_1996_约瑟夫问题.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+
+    list<int> l;
+    for(int i=1;i<=n;i++) l.push_back(i);
+    list<int>::iterator it=l.begin();
+    while(!l.empty())
+    {
+        for(int i=1;i<m;i++)
+        {
+            it++;
+            if(it==l.end())
+                it=l.begin();
+        }
+            cout<<*it<<" ";
+            list<int>:: iterator next;
+            next=++it;
+            l.erase(--it);
+            if(next==l.end()) next=l.begin();
+            it=next;
+       
+    }
+    
+    return 0;
+}
