d103a206e00073a0232f4bec41cbdb037634453c
LuYuxuan <ASUS@LAPTOP-T9K3FD3O>
2026-04-08 07:57:54 +0800

    前缀和第3次更改——还是超时

--- Code/16_k倍区间.cpp
+++ Code/16_k倍区间.cpp
@@ -21,33 +21,51 @@ int main()
   //           }
   //       }
   //   }
+  // int n,m;
+  // cin>>n>>m;
+  // vector<int> nums(n);
+  // for(int i = 0;i < n;i++) cin>>nums[i];
+  // int cnt = 0;
+
+  // //前缀和数组
+  // vector<long long> preSum(n);
+  // preSum[0] = nums[0];
+  // for(int i = 1;i < n;i++) preSum[i] = preSum[i-1] +nums[i];
+
+  // for(int i = 0;i < n;i++)
+  // {
+  //   for(int j = i;j < n; j++)
+  //   {
+  //     if(i == 0 && j == i) 
+  //     {
+  //       if(preSum[i] % m == 0) cnt++;
+  //     }
+  //     else if(i == 0)
+  //     {
+  //       if(preSum[j] % m == 0) cnt++;
+  //     }
+  //     else
+  //     {
+  //       if((preSum[j] - preSum[i-1]) % m == 0) cnt++;
+  //     }
+  //   }
+  // }
+
+
   int n,m;
   cin>>n>>m;
   vector<int> nums(n);
   for(int i = 0;i < n;i++) cin>>nums[i];
-  int cnt = 0;
+  long long cnt = 0;
+  long long sum = 0;
+  //前缀和数组  
+  //为了避免else if (i == 0)的情况，我们可以在前缀和数组的开头添加一个元素0，这样preSum[i]就表示前i个元素的和，preSum[0] = 0。
+  vector<long long> preSum(n + 1, 0); // 长度为 n+1
+  for(int i = 1; i <= n; i++) preSum[i] = preSum[i-1] + nums[i-1];
 
-  //前缀和数组
-  vector<long long> preSum(n);
-  preSum[0] = nums[0];
-  for(int i = 1;i < n;i++) preSum[i] = preSum[i-1] +nums[i];
-
-  for(int i = 0;i < n;i++)
-  {
-    for(int j = i;j < n; j++)
-    {
-      if(i == 0 && j == i) 
-      {
-        if(preSum[i] % m == 0) cnt++;
-      }
-      else if(i == 0)
-      {
-        if(preSum[j] % m == 0) cnt++;
-      }
-      else
-      {
+  for(int i = 1; i <= n; i++) {
+    for(int j = i; j <= n; j++) {
         if((preSum[j] - preSum[i-1]) % m == 0) cnt++;
-      }
     }
   }
 
